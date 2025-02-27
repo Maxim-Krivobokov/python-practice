4d7970261561b5fef8d7ed98dcf23cdd4c075cd0
Maxim Krivobokov <maxim.krivobokov@gmail.com>
2025-02-27 17:31:20 +0100

    updated flask example

--- engineering/10.APIs/Flask/README.md
+++ engineering/10.APIs/Flask/README.md
@@ -9,3 +9,29 @@ export FLASK_APP=main
 flask run
 ```
 
+### How to test local webserver:
+example with args, tokens, cookies:
+```sh
+curl "localhost:5000/course/cpp/unit/002.1?sort=id&order=asc" -H "x-auth-token: ahdgcajhsdgasjhgd2132" --cookie "ga=2376452367452367"
+```
+example PUT request, to add the course:
+
+```sh
+curl "localhost:5000/course" -X PUT -d '{"name": "kafka", "desc": "apache kafka installation"}' -H "Content-Type: application/json"
+```
+
+## Working in async mode;
+### Gunicorn
+install with pip: ```pip3 install gunicorn```
+
+How to launch:
+```sh
+guniciorn -w 4 -b 0.0.0.0:4000 main:app
+# -w 4 = 4 workers
+# -b = bind address and port
+# main.py = main, application is called app;
+```
+Important!
+Gunicorn won't work with scripts, that stores data in internal dict-list objects, like COURSES = {}
+Each worker will have own COURSES;
+Must use external storage (DB for example)
--- engineering/10.APIs/Flask/main.py
+++ engineering/10.APIs/Flask/main.py
@@ -1,14 +1,52 @@
 # flask is installed in WSL; or use venv
-from flask import Flask 
+from flask import Flask, request, jsonify
+from logging.config import dictConfig
+
+# Defining format of logs
+dictConfig({
+    "version": 1,
+    "formatters": { "default": {
+        "format": '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    }},
+    "handlers": {
+        "wsgi": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "default",
+        }
+    },
+    "root": {
+        "level": "DEBUG",
+        "handlers": ['wsgi']
+    }
+})
 
 app = Flask(__name__)
+
+# That type of storage for testing purpose only
+COURSES = {}
 
 @app.route('/')
 def hello_world():
     return "Hello world\n"
 
 # templated params are defined with <variable_name>
-# also we can pre-define type: <int:var_name> - if string will be passed, error '404' will occur
-@app.route("/course/<course_name>/unit/<int:unit_id>")
-def show_course_by_name(course_name, unit_id):
-    return f"course {type(course_name)} {course_name} and unit {type(unit_id)} {unit_id}\n", 202
+# also we can pre-define type: <int:var_name> - if other type will be passed, error '404' will occur
+@app.route("/course/<course_name>", methods=["GET", "DELETE"])
+def show_course_by_name(course_name):
+    if request.method == "GET":
+        app.logger.info(request.args)
+        app.logger.info(request.headers)
+        app.logger.info(request.cookies)
+        return COURSES[course_name]
+    if request.method == "DELETE":
+        del COURSES[course_name]
+        return jsonify({"status": "ok"})
+
+@app.route("/course", methods=["PUT"])
+def create_course():
+    app.logger.info(request.get_data())
+    app.logger.info(request.is_json)
+    payload = request.json
+    COURSES[payload["name"]] = payload["desc"]
+    return jsonify({"status": "ok"})
